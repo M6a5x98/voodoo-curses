b1475cdf124ec7bb4be2829d8480ff652d2e2d20
M6a5x98 <m6a5x98@outlook.com>
2025-09-10 18:19:19 +0200

    v1.1.0

--- src/main/java/m6a5x98/voodoocurses/VoodooCurses.java
+++ src/main/java/m6a5x98/voodoocurses/VoodooCurses.java
@@ -2,8 +2,10 @@ package m6a5x98.voodoocurses;
 
 import m6a5x98.voodoocurses.block.ModBlocks;
 import m6a5x98.voodoocurses.item.ModItems;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
@@ -14,7 +16,9 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VoodooCurses implements ModInitializer {
+import static m6a5x98.voodoocurses.VoodooCursesUtils.FIRST_CURSE_AT;
+
+public class VoodooCurses implements ModInitializer, ClientModInitializer {
 	public static final String MOD_ID = "voodoo-curses";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
@@ -32,5 +36,20 @@ public class VoodooCurses implements ModInitializer {
 				fabricLootSupplierBuilder.pool(cloverPool);
 			}
 		});
+	}
+
+	@Override
+	public void onInitializeClient() {
+		ModelPredicateProviderRegistry.register(
+				ModItems.VOODOO_DOLL,
+				new Identifier("cursed_or_blessed"),
+				(stack, world, entity, seed) -> {
+					if (!stack.getOrCreateNbt().contains("CurseOrBlessing")) return 0.0f;
+					return stack.getOrCreateNbt().getInt("CurseOrBlessing") < FIRST_CURSE_AT
+							? 1.0f
+							: 0.5f;
+
+				}
+		);
 	}
 }
